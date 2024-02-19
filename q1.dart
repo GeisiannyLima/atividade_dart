3aab54a18fbd1fd9a4dfab48bcb27cb498c24f84
GeisiannyLima <112423224+GeisiannyLima@users.noreply.github.com>
2024-02-19 00:33:12 -0300

    Update q1.dart

--- q1.dart
+++ q1.dart
@@ -1,16 +1,18 @@
 void main() {
-  //Declaração e incialização dos vetores
+  //Declaração e incialização da Lista com salários
   List<double> salarios = [1200, 1000, 600, 1100, 900];
-  
+  // Criação de uma lista com nomes
   List<String> nomes = ["Maria", "Joao", "Antonia", "Sandra", "Jeova"];
+  // Ordenação da lista de salários de forma crescente
   salarios.sort();
   print(salarios);
-
+   // Ordenação da lista de salários de forma decrescente
   List<double> decrescente = salarios.reversed.toList();
   print("Decrescente: ");
   print(decrescente);
-
+   // Ordenação da lista de nomes em ordem alfabética 
   nomes.sort();
   print("Nomes em ordem alfabética> ");
   print(nomes);
+  
 }
